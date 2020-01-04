b69859f41c03d90a460c8367a52a3a268747330b
Peter Huang <peterhuang@Peters-MacBook-Pro.local>
2020-01-04 15:58:14 -0800

    tryna remember hwo to sort a hashmap

---
+++ hashmap_sort/sort_hashmap.java
@@ -0,0 +1,32 @@
+import java.util.*;
+import java.util.Map.Entry;
+
+public class sort_hashmap {
+	public static void main(String[] args) {
+		HashMap<Integer, String> map = new HashMap<Integer, String>();
+
+		map.put(5, "ant");
+		map.put(2, "lol");
+		map.put(3, "excalibur");
+		map.put(1, "meme");
+
+		Comparator<Entry<Integer, String>> comp = new Comparator<Entry<Integer, String>>() {
+			@Override
+			public int compare(Entry<Integer, String> e1, Entry<Integer, String> e2) {
+				String v1 = e1.getKey().toString();
+				String v2 = e2.getKey().toString();
+				return v1.compareTo(v2);
+			}
+		};
+
+		// get Entry Set
+		Set<Entry<Integer, String>> entries = map.entrySet();
+		List<Entry<Integer, String>> listEntries = new ArrayList<Entry<Integer, String>>(entries);
+		
+		Collections.sort(listEntries, comp);
+		
+		for (Entry<Integer, String> ent : listEntries) {
+			System.out.println(ent.getKey() + " " + ent.getValue());
+		}
+	}
+}
